9d1f605d1682c2c012c0e30112f785fadcddf720
Egor Ciulcov <chulkov.e@phystech.edu>
2022-09-29 13:51:52 +0300

    create project

---
+++ src/task_1_1_9.cpp
@@ -0,0 +1,12 @@
+/*
+ * 1.1.9. Последовательность Фибоначчи определяется так: a0 = 0,
+					a1 = 1, ak = ak-1 + ak-2 при k > 2. Дано n, вычислить an.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl;
+	return 0;
+}
